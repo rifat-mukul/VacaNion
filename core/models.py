a7109ca61bd99ae778f41ddfe16047f5ea5445dd
riadkabir45 <riadkabir45@gmail.com>
2024-09-13 16:01:46 +0600

    fix: dup model

--- core/models.py
+++ core/models.py
@@ -51,15 +51,7 @@ class Booked(models.Model):
 
     def __str__(self):
         return self.user.username + " -> " + self.hotel.name + "@" + str(self.book_date)
-class ReviewRatings(models.Model):
-    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-    hotel = models.ForeignKey(Hotel,on_delete=models.CASCADE)
-    ratings = models.FloatField()
-    review = models.TextField()
 
-
-    def __str__(self):
-            return f"{self.user.username} -> {self.hotel.name}: {self.ratings}/5 - {self.review}"
 
 class ChatTable(models.Model):
     booking = models.ForeignKey(Booked,on_delete=models.CASCADE)
